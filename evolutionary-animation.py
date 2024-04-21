6e01df4d20acbea0f8bff438b592b113020f8e56
joshrico <98072813+joshrico@users.noreply.github.com>
2024-04-20 20:48:17 -1000

    Created first iteration of play_animation()
    - directly queries database by query

--- evolutionary-animation.py
+++ evolutionary-animation.py
@@ -5,6 +5,9 @@ import random
 import sqlite3
 import os
 
+db_path = os.path.join('D:\Code\evolutionary-animation', 'creatures.db')
+    
+
 def create_database(db_path):
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
@@ -56,18 +59,14 @@ def query_by_generation(db_path, generation):
     conn.close()
     return results
 
-def makeLeg(w, h, d):
-    legPart = cmds.polyCube(width=w, height=h, depth=d)
-    return legPart
+def makePart(w, h, d, name):
+    part = cmds.polyCube(width=w, height=h, depth=d)
+    cmds.rename(part[0], name)
+    return part[0]
 
 def makeFloor():
     floor = cmds.polyPlane(width=500, height=500)
     return floor
-
-
-def makeBody(bodyW, bodyH, bodyD):
-    body = cmds.polyCube( width=bodyW, height=bodyH, depth=bodyD)
-    return body
 
 def createCreature(bodyW, bodyH, bodyD, legW, legH, legD, spinImp, initVel, idx, idx2, rigidS, gravField):
     legs = []
@@ -75,11 +74,11 @@ def createCreature(bodyW, bodyH, bodyD, legW, legH, legD, spinImp, initVel, idx,
     pins = []
 
     # create body and move it accordingly
-    body = makeBody(bodyW, bodyH, bodyD)
     bodyName = 'body' + str(idx)
-    cmds.select(body)
-    cmds.rigidBody(act=True, slv=rigidS, m=20, iv=initVel, n=bodyName)
-    cmds.move(0, 4, 0, body)
+    makePart(bodyW, bodyH, bodyD, bodyName)
+    cmds.select('body'+str(idx))
+    cmds.rigidBody(act=True, slv=rigidS, m=20, iv=initVel, n='rigid'+bodyName)
+    cmds.move(0, 4, 0, bodyName)
 
     # store positions into an array after calculations
     legPos.append((((bodyW/2)-1.5), (bodyH/6), (legD/2+(bodyD/2))))
@@ -91,29 +90,32 @@ def createCreature(bodyW, bodyH, bodyD, legW, legH, legD, spinImp, initVel, idx,
     for i in range(0,4):
         legName = 'calf' + str(idx2+i)
         pinName = 'pin' + str(idx2+i)
-        legs.append(makeLeg(legW, legH, legD))
-        cmds.rotate(0, 0, '90deg', legs[i])
+        makePart(legW, legH, legD, legName)
+        legs.append(legName)
+        cmds.rotate(0, 0, '90deg', legName)
 
         # move legs
-        cmds.move(legPos[i][0], legPos[i][1], legPos[i][2], legs[i])
+        cmds.move(legPos[i][0], legPos[i][1], legPos[i][2], legName)
 
         # make sure each leg is a rigidBody
-        cmds.select(legs[i])
-        cmds.rigidBody(act=True, b=0, slv=rigidS, imp=legPos[i], si=spinImp, m=5, damping=0.4, n=legName)
+        cmds.select(legName)
+        cmds.rigidBody(act=True, b=0, slv=rigidS, imp=legPos[i], si=spinImp, m=5, damping=0.4, n='rigid'+legName)
 
         pins.append(cmds.constrain(legName, bodyName, hinge=True, n=pinName, p=legPos[i]))
 
         cmds.connectDynamic(legName, f=gravField)
 
-    creature = cmds.group(body, *legs, *pins, n='creature'+str(idx))
+    creature = cmds.group(bodyName, *legs, *pins, n='creature'+str(idx))
 
     return creature
 
-def test_generation():
-
-    shift1 = cmds.getAttr('pCube1.translateX')
-    shift2 = cmds.getAttr('pCube6.translateX')
-    shift3 = cmds.getAttr('pCube11.translateX')
+def play_animation():
+    print('fuck')
+    gen = query_by_generation(db_path, 1)
+    print(gen)
+    shift1 = cmds.getAttr(gen[0][1]+'.translateX')
+    shift2 = cmds.getAttr(gen[1][1]+'.translateX')
+    shift3 = cmds.getAttr(gen[2][1]+'.translateX')
 
     print(0)
     print(shift1)
@@ -124,9 +126,9 @@ def test_generation():
     cmds.playbackOptions( minTime='0sec', maxTime='10sec' )
     cmds.play(w=True)
 
-    shift1 = cmds.getAttr('pCube1.translateX')
-    shift2 = cmds.getAttr('pCube6.translateX')
-    shift3 = cmds.getAttr('pCube11.translateX')
+    shift1 = cmds.getAttr(gen[0][1]+'.translateX')
+    shift2 = cmds.getAttr(gen[1][1]+'.translateX')
+    shift3 = cmds.getAttr(gen[2][1]+'.translateX')
     print(12)
     print(shift1)
     print(shift2)
@@ -138,13 +140,13 @@ def create_generic_gui():
     if cmds.window(window_name, query=True, exists=True):
         cmds.deleteUI(window_name)
 
-    cmds.window(window_name, title="Generic Functions Interface", widthHeight=(300, 150))
+    cmds.window(window_name, title=window_name, widthHeight=(300, 150))
     cmds.columnLayout(adjustableColumn=True)
 
     # Buttons for different functions
     cmds.button(label="Create Generation", command=lambda x: print('placeholder for create generation'))
     cmds.button(label="Store Generation", command=lambda x: print('placeholder for store generation'))
-    cmds.button(label="Test Generation", command=lambda x: test_generation())
+    cmds.button(label="Test Generation", command=lambda x: play_animation())
 
     cmds.showWindow(window_name)
 
@@ -152,9 +154,6 @@ def create_generic_gui():
 def main():
     cmds.file(new=True, force=True) # make a new scene and don't ask for confirmation
 
-    # Path to the database file
-    db_path = os.path.join('D:\Code\evolutionary-animation', 'creatures.db')
-    print(db_path)
     create_database(db_path)
     query_polyshapes(db_path)
     creatures = []
@@ -186,29 +185,24 @@ def main():
     gravityField = cmds.gravity(pos=(0, 0, 0), m=9.8, dx=0, dy=-1, dz=0, name='gravityField')
 
 
-    # cmds.setAttr('body.initialVelocityX', 1)
-    # cmds.setAttr('calf.spinImpulseZ', 10)
-
-    # Run the animation
-    cmds.playbackOptions( minTime='0sec', maxTime='15sec' )
-
     initialGeneration = []
 
-    for i in range(0,3):
+    for i in range(3):  # Simplified loop header
         randomNums = []
-        for j in range(0, 12):
+        for j in range(12):
             if j < 3:
-                randomNums.append(random.uniform(8,20))
-            elif j >= 3 and j < 5:
-                randomNums.append(random.uniform(randomNums[1]/2,randomNums[1]))
+                randomNums.append(random.uniform(8, 20))
+            elif j < 5:
+                randomNums.append(random.uniform(randomNums[1] / 2, randomNums[1]))
             elif j == 5:
-                randomNums.append(random.uniform(randomNums[1]/2,4))
+                randomNums.append(random.uniform(randomNums[1] / 2, 4))
             else:
                 randomNums.append(random.uniform(1, 5))
+
         initialGeneration.append(randomNums)
         print(initialGeneration[i])
 
-        creatures.append(createCreature(
+        creature_id = createCreature(
             initialGeneration[i][0],
             initialGeneration[i][1],
             initialGeneration[i][2],
@@ -217,19 +211,24 @@ def main():
             initialGeneration[i][5],
             (0, 0, -(initialGeneration[i][8])),
             (0, 0, 0),
-            i+1,
-            i*4,
-            rigidSolver,
+            i + 1,
+            i * 4,
+            rigidSolver,  # Assuming these are placeholders
             gravityField
-        ))
+        )
 
+        # Add the creature to the database with distance traveled as 0
+        add_polyshape(db_path, creature_id, 1,  # Assuming generation 1
+                    initialGeneration[i][0], initialGeneration[i][1], initialGeneration[i][2],
+                    initialGeneration[i][3], initialGeneration[i][4], initialGeneration[i][5], 0)
+
+        # Move logic in Maya (assumes use of cmds, which needs to be defined/imported if using outside of Maya)
         if i == 0:
-            cmds.move(0, initialGeneration[i][4]*2, -50, creatures[i])
+            cmds.move(0, initialGeneration[i][4] * 2, -50, creature_id)
         elif i == 1:
-            cmds.move(0, initialGeneration[i][4]*2, 0, creatures[i])
+            cmds.move(0, initialGeneration[i][4] * 2, 0, creature_id)
         else:
-            cmds.move(0, initialGeneration[i][4]*2, 50, creatures[i])
-
+            cmds.move(0, initialGeneration[i][4] * 2, 50, creature_id)
 
 
 
@@ -239,6 +238,8 @@ def main():
 
 
     create_generic_gui()
+
+    print(query_polyshapes(db_path))
 
 if __name__ == "__main__":
     main()
